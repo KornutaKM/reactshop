b61c2f20b765383153fd53a100a47e3bd96e8f27
Koxypo <onemorekorn@gmail.com>
2023-05-31 19:02:21 +0500

    project complete upd header&footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
       <div className='footer-copyright'>
         <div className='container'>
           © {new Date().getFullYear()} Copyright
-          <a className='grey-text text-lighten-4 right' href='#!'>
+          <a
+            href='https://kornutakm.github.io/React-shop/'
+            target='_blank'
+            rel='noreferrer'
+          >
             Repo
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ const Header = () => {
   return (
     <nav className='#5c6bc0 indigo lighten-1'>
       <div className='nav-wrapper'>
-        <a href='#!' className='brand-logo'>
+        <a href='/' className='brand-logo'>
           React Shop
         </a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <a href='#!'>Repo</a>
+            <a href='https://kornutakm.github.io/React-shop/' target='_blank' rel="noreferrer">Repo</a>
           </li>
         </ul>
       </div>
